1e4d0d36f0c27ab0ac88133d82f89deb21dff584
Joshf225 <93196964+Joshf225@users.noreply.github.com>
2023-08-29 15:31:59 +0100

    commented out listen function

--- server/index.js
+++ server/index.js
@@ -30,31 +30,31 @@ mongoose
     console.log(err.message);
   });
 
-const server = app.listen(3000, () => {
-  console.log("SERVER STARTED ON 3000");
-});
+// const server = app.listen(3000, () => {
+//   console.log("SERVER STARTED ON 3000");
+// });
 
-const io = socket(server, {
-  cors: {
-    origin: "https://localhost:5000",
-    credetentials: true,
-  },
-});
+// const io = socket(server, {
+//   cors: {
+//     origin: "https://localhost:5000",
+//     credetentials: true,
+//   },
+// });
 
-global.onlineUsers = new Map();
+// global.onlineUsers = new Map();
 
-io.on("connection", (socket) => {
-  global.chatSocket = socket;
-  //setting userId and socket id to global map
-  socket.on("add-user", (userId) => {
-    onlineUsers.set(userId, socket.id);
-  });
+// io.on("connection", (socket) => {
+//   global.chatSocket = socket;
+//   //setting userId and socket id to global map
+//   socket.on("add-user", (userId) => {
+//     onlineUsers.set(userId, socket.id);
+//   });
 
-  socket.on("send-msg", (data) => {
-    const sendUserSocket = onlineUsers.get(data.to);
-    //checking if msg sending to a user is online then emitting msg to user and storing msg in database,if not its stored into database
-    if (sendUserSocket) {
-      socket.to(sendUserSocket).emit("msg-recieve", data.message);
-    }
-  });
-});
+//   socket.on("send-msg", (data) => {
+//     const sendUserSocket = onlineUsers.get(data.to);
+//     //checking if msg sending to a user is online then emitting msg to user and storing msg in database,if not its stored into database
+//     if (sendUserSocket) {
+//       socket.to(sendUserSocket).emit("msg-recieve", data.message);
+//     }
+//   });
+// });
