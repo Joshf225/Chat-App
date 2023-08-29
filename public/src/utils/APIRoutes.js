f5cf01ad9ad379902474316d81e3719badfb5599
Joshf225 <93196964+Joshf225@users.noreply.github.com>
2023-08-29 20:31:27 +0100

    added new host link

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "https://chat-app-frontend-jy6c.onrender.com";
+export const host =
+  "https://64ee4712cfbe91209fe26bda--startling-gingersnap-4fe396.netlify.app";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
--- server/index.js
+++ server/index.js
@@ -35,7 +35,8 @@ const server = app.listen(3000, () => {
 
 const io = socket(server, {
   cors: {
-    origin: "https://chat-app-frontend-jy6c.onrender.com/",
+    origin:
+      "https://64ee4712cfbe91209fe26bda--startling-gingersnap-4fe396.netlify.app/",
     credetentials: true,
   },
 });
